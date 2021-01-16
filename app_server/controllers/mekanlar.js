d6272825d13e4ba7fd0655d3e64a68d1a6da2c46
Muaz Erdem Yiğit <muazerdemyigit@hotmail.com>
2021-01-16 17:17:09 +0300

    Son değişiklikler

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -2,7 +2,7 @@ var express = require("express");
 var request = require("postman-request");
 
 var apiSecenekleri = {
-  sunucu: "https://muazerdemyigit1821012046.herokuapp.com/",
+  sunucu: "https://localhost:3000",
   apiYolu: "/api/mekanlar/",
 };
 
